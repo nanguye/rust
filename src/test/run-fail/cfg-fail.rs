105074b1098b2faff6eced00f31dfdf16f8ea5df
Nam Nguyen <nanguye@microsoft.com>
2019-12-27 22:54:46 -0800

    Fix cfg-fail test

--- src/test/run-fail/cfg-fail.rs
+++ src/test/run-fail/cfg-fail.rs
@@ -1,5 +1,5 @@
-// no-system-llvm
-// compile-flags: -C control-flow-guard=checks
+// compile-flags: -C control-flow-guard=checks -C debuginfo=2 -C opt-level=0
+// error-pattern:
 // failure-status: -1073740791
 
 type FunPtr = fn(i32)->i32;
